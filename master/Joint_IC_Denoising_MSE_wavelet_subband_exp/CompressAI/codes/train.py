a913f80d14445792dc1a378666a8f50099241840
naoryo2910 <naoryo2910@users.noreply.github.com>
2026-05-29 18:58:09 +0900

    Save stage-wise wavelet subband experiment

--- master/Joint_IC_Denoising_MSE_wavelet_subband_exp/CompressAI/codes/train.py
+++ master/Joint_IC_Denoising_MSE_wavelet_subband_exp/CompressAI/codes/train.py
@@ -3,6 +3,9 @@ import math
 import argparse
 import random
 import logging
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import torch
 import torch.distributed as dist
@@ -418,4 +421,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
